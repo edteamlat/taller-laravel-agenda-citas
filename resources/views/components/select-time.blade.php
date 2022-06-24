5884d8dc7024b4aeb002b29e7bc30c042f7544ce
Jairo Ushiña <jago86@gmail.com>
2022-06-24 01:30:48 -0500

    Mejoras y correcciones en views.

--- resources/views/components/select-time.blade.php
+++ resources/views/components/select-time.blade.php
@@ -8,11 +8,15 @@
 <select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
     <option>--Selecciona la hora--</option>
     @foreach (range($initHour, $endHour) as $hour)
-        <option value="{{ "{$initHour}:00" }}" {{ "{$initHour}:00" == $selectedHour ? 'selected' : '' }}>
-            {{ "{$hour}:00" }}
+        @php
+            $formatedHour = str_pad($hour, 2, '0', STR_PAD_LEFT);
+        @endphp
+
+        <option value="{{ $formatedHour.":00" }}" {{ "{$formatedHour}:00" == $selectedHour ? 'selected' : '' }}>
+            {{ "{$formatedHour}:00" }}
         </option>
-        <option value="{{ "{$initHour}:30" }}" {{ "{$initHour}:30" == $selectedHour ? 'selected' : '' }}>
-            {{ "{$hour}:30" }}
+        <option value="{{ $formatedHour.":30" }}" {{ "{$formatedHour}:30" == $selectedHour ? 'selected' : '' }}>
+            {{ "{$formatedHour}:30" }}
         </option>
     @endforeach
 </select>
--- resources/views/my-schedule/create.blade.php
+++ resources/views/my-schedule/create.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-
                     <form action="{{ route('my-schedule.store') }}" method="POST">
                         @csrf
                         <div class="grid grid-cols-2 gap-4">
@@ -18,22 +17,22 @@
                             <div>
                                 <x-label for="from[date]" :value="__('Fecha para la cita')" />
 
-                                <x-input id="from[date]" class="block mt-1 w-full" type="date" name="from[date]" :value="old('from[date]', request('date'))" autofocus />
+                                <x-input id="from[date]" class="block mt-1 w-full" type="date" name="from[date]" :value="old('from.date', request('date'))" autofocus />
                             </div>
 
                             <div>
                                 <x-label for="from[time]" :value="__('Elije la hora de inicio')" />
 
-                                <x-select-time id="from[time]" init-hour="8" end-hour="17" :selected-hour="old('from[time]')" class="block mt-1 w-full" name="from[time]"></x-select-time>
+                                <x-select-time id="from[time]" init-hour="8" end-hour="17" :selected-hour="old('from.time')" class="block mt-1 w-full" name="from[time]"></x-select-time>
                             </div>
 
                             <div>
                                 <x-label for="service_id" :value="__('Elige el servicio')" />
 
-                                <x-select id="service_id" init-hour="8" end-hour="17" :selected="old('service_id')" class="block mt-1 w-full" name="service_id">
+                                <x-select id="service_id" class="block mt-1 w-full" name="service_id">
                                     <option value="">--Selecciona el servicio--</option>
                                     @foreach ($services as $service)
-                                        <option value="{{ $service->id }}">{{ $service->name }}</option>
+                                        <option value="{{ $service->id }}" {{ old('service_id') == $service->id ? 'selected' : '' }}>{{ $service->name }}</option>
                                     @endforeach
                                 </x-select>
                             </div>
@@ -41,10 +40,10 @@
                             <div>
                                 <x-label for="staff_user_id" :value="__('Elige quién te atenderá')" />
 
-                                    <x-select id="staff_user_id" init-hour="8" end-hour="17" :selected-hour="old('staff_user_id')" class="block mt-1 w-full" name="staff_user_id">
+                                    <x-select id="staff_user_id" class="block mt-1 w-full" name="staff_user_id">
                                         <option value="">--Selecciona quien te atenderá--</option>
                                         @foreach ($staffUsers as $user)
-                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                            <option value="{{ $user->id }}" {{ old('staff_user_id') == $user->id ? 'selected' : '' }}>{{ $user->name }}</option>
                                         @endforeach
                                     </x-select>
                             </div>
