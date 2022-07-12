289d1bd4d459c8076f3bc240a25ed8a663611c63
Jairo Ushiña <jago86@gmail.com>
2022-07-12 17:33:15 -0500

    M4. Leccion 5.

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-      Tailwind Starter Template - Landing Page Template: Tailwind Toolbox
+      <title>{{ config('app.name', 'Laravel') }} {{ isset($title) ? " - {$title}" : "" }}</title>
     </title>
     <meta name="description" content="Simple landind page" />
     <meta name="keywords" content="" />
